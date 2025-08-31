2c0be330376d98a3fa10d0e008c17cf9d5fa3a7d
bytecod3 <emwiti658@gmail.com>
2025-08-31 15:44:02 +0300

    adds state pattern

--- troll-fsm.cpp
+++ troll-fsm.cpp
@@ -15,8 +15,32 @@ class Troll {
 
 // abstract class as the base class for different states
 class State {
-    public: 
+    public:
         virtual void Execute(Troll* troll) = 0;
 };
 
-// derived class of the State base class
+// STATES 
+
+// state runAway 
+class State_RunAway : public State {
+    void Execute(Troll* troll) override {
+        // if the troll is safe, sleep, otherwise move away from enemy
+        if(troll->is_safe()) {
+            troll->change_state(new State_Sleep());
+        } else {
+            troll->move_away_from_enemy();
+        }
+    }
+};
+
+// state sleep
+class State_Sleep : public State {-
+    public:
+        void Execute(Troll* troll) {
+            if(troll->is_threatened()) {
+                troll->change_state(new State_RunAway());
+            } else {
+                troll->snore();
+            }
+        }
+};
